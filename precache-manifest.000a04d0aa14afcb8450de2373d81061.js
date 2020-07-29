a1852ad86470f22b7fee6697d6c27f45fd24a6fb
Oscar Rodriguez <oscarrobertrodriguez@gmail.com>
2020-07-28 21:03:35 -0500

    Updates

---
+++ precache-manifest.000a04d0aa14afcb8450de2373d81061.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1488a62323395ad0a0942b8877af4f75",
+    "url": "/OscarRobertRodriguez/borderRadiusPreviewer/index.html"
+  },
+  {
+    "revision": "079c89127449a74a54d3",
+    "url": "/OscarRobertRodriguez/borderRadiusPreviewer/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e81d4551adec9dd144db",
+    "url": "/OscarRobertRodriguez/borderRadiusPreviewer/static/js/2.1b5dd36d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/OscarRobertRodriguez/borderRadiusPreviewer/static/js/2.1b5dd36d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "079c89127449a74a54d3",
+    "url": "/OscarRobertRodriguez/borderRadiusPreviewer/static/js/main.02e9c2fd.chunk.js"
+  },
+  {
+    "revision": "128a72a6957a80db2b41",
+    "url": "/OscarRobertRodriguez/borderRadiusPreviewer/static/js/runtime-main.72bb1435.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/OscarRobertRodriguez/borderRadiusPreviewer/static/media/logo.5d5d9eef.svg"
+  }
+]);
